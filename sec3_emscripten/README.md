b3861b0c6503dad85636bcac308b7d1d1dd7c99d
Matheus Jardim <matheus.jardim@conceptu.ind.br>
2023-06-19 15:43:47 -0300

    export C function to JS

--- sec3_emscripten/README.md
+++ sec3_emscripten/README.md
@@ -6,3 +6,11 @@ To build the .c files, run `./compile.sh`
 
 Use `npm run start` to run this project. It'll be available at http://localhost:2222/.
 Check the browser's console for debugging info.
+
+# Snippets
+
+```js
+ccall('main')
+ccall('greet', 'string', ['string'], ['matheus'])
+ccall('getDoubleNum', 'number', ['number'], [12])
+```
--- sec3_emscripten/compile.sh
+++ sec3_emscripten/compile.sh
@@ -1,4 +1,7 @@
 #emcc lib/program.c -s WASM=1 -o public/demo.js
 #emcc lib/program.c -s WASM=1 -o public/demo.html
 #emcc lib/program.c -s WASM=1 -s SIDE_MODULE=1 -o public/demo.wasm
-emcc lib/program.c -s WASM=1 -O3 -o public/demo.js
+#emcc lib/program.c -s WASM=1 -O3 -o public/demo.js
+
+# add '_' to function names
+emcc lib/program.c -s WASM=1 -s EXPORTED_FUNCTIONS="['_getNum','_main','_greet','_getDoubleNum']" -s EXPORTED_RUNTIME_METHODS="['ccall']" -O3 -o public/demo.js
