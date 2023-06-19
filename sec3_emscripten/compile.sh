c34f8d4ccc4c4befe27758ea27cda235de800cd9
Matheus Jardim <matheus.jardim@conceptu.ind.br>
2023-06-19 15:51:22 -0300

    cwrap

--- sec3_emscripten/compile.sh
+++ sec3_emscripten/compile.sh
@@ -4,4 +4,5 @@
 #emcc lib/program.c -s WASM=1 -O3 -o public/demo.js
 
 # add '_' to function names
-emcc lib/program.c -s WASM=1 -s EXPORTED_FUNCTIONS="['_getNum','_main','_greet','_getDoubleNum']" -s EXPORTED_RUNTIME_METHODS="['ccall']" -O3 -o public/demo.js
+#emcc lib/program.c -s WASM=1 -s EXPORTED_FUNCTIONS="['_getNum','_main','_greet','_getDoubleNum']" -s EXPORTED_RUNTIME_METHODS="['ccall','cwrap']" -O3 -o public/demo.js
+emcc lib/program.c -s WASM=1 -s EXPORTED_FUNCTIONS="['_getNum','_main','_greet','_getDoubleNum']" -s EXPORTED_RUNTIME_METHODS="['ccall','cwrap']" -o public/demo.js
